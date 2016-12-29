e92a283230882b104ebeb95639505efafb5afee8
k--kato <keisuke.k.kato@gmail.com>
2016-12-29 22:28:19 +0900

    fixed Parameters not correctly identified when documenting constructor which chains to a base class constructor. #22

--- test/TestData/X.cs
+++ test/TestData/X.cs
@@ -16,9 +16,9 @@ namespace N  // "N:N"
         public event D d; // "E:N.X.d"
         public int this[string s]{get{return 1;}} // "P:N.X.Item(System.String)"
         public class Nested{} // "T:N.X.Nested"
-        public delegate void D(int i); // "T:N.X.D"        
+        public delegate void D(int i); // "T:N.X.D"
         public static explicit operator int(X x){return 1;} // "M:N.X.op_Explicit(N.X)~System.Int32"
-        public DbSet<Director> Directors { get; set; }        
+        public DbSet<Director> Directors { get; set; }
         public List<int> bb<int>(string s, ref List<int> y, void * z){return 1;} // "M:N.X.bb(System.String,System.Int32@,=System.Void*)"
         int bb(string s, ref int y, void * z){return 1;} // "M:N.X.bb(System.String,System.Int32@,=System.Void*)"
         int Generate(int level);
@@ -29,5 +29,7 @@ namespace N  // "N:N"
         public async Task<string> GetInfoForTime(string location, double time)
         public Collection<T> Filter(Func<T, bool> query) { }
         public Collection<T> Filter(Func<T, bool> queryFirst, Func<T, U, V> querySecond) { }
+        public Test(string options) : base(options)
+        public Testing2(string String1, string String2, string String3) : base()
    }
 }
