f74d764ae05dbe323da580d3bee4dcb4db04c542
k--kato <keisuke.k.kato@gmail.com>
2017-01-31 00:50:03 +0900

    Fixed #32 Parameters are not returned

--- src/SyntacticAnalysis/SyntacticAnalysisCSharp.ts
+++ src/SyntacticAnalysis/SyntacticAnalysisCSharp.ts
@@ -90,7 +90,8 @@ export class SyntacticAnalysisCSharp {
 
     public static GetMethodParamNameList(code: string): Array<string> {
         if (code === null) return null;
-        const params: RegExpMatchArray = code.match(/[\w\S]\s+[\w\S]+\s*\(([^)]*)\)/);
+        const removedAttrCode: string = code.replace(/^\s*\[.+?\]/, ''); // FIXME:
+        const params: RegExpMatchArray = removedAttrCode.match(/[\w\S]\s+[\w\S]+\s*\(([^)]*)\)/);
 
         const isMatched = (params === null || params.length !== 2);
         if (isMatched) return null;
